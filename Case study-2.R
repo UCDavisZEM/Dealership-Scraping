7736ff9d072c4acee14f20aa1176b98d44eccb26
jianshi <jshzhang@ucdavis.edu>
2015-07-29 14:58:22 -0700

    search/new/tp/

--- Case study-2.R
+++ Case study-2.R
@@ -2,7 +2,7 @@ url = "http://www.vacavillehonda.com/search/new/tp/"
 #doc = htmlParse(url)
 #baselink = substr(url, 1, gregexpr("/",url)[[1]][3]-1)
 
-
+url = "http://www.harr.com/search/new/tp/"
 
 #grab the linklist
 
@@ -22,13 +22,11 @@ getLinklist.2 = function(url){
   index = gregexpr("/",url)[[1]]
   pattern = substr(url, index[3],nchar(url) )
   temp = href[grep(pattern,href)]
-  
   ind = grep("p:", gsub(pattern, "", temp))
   #get last page's link
   lastpage = paste0(baselink, temp[ind[length(ind)]])
   #get the number of total pages from the last page link
-  lastpagenumber = as.numeric(substr(lastpage, gregexpr("p:", lastpage)[[1]][2]+2,
-                                     gregexpr("p:", lastpage)[[1]][2]+2))
+  lastpagenumber = as.numeric(gsub(".*p:([0-9]+).*", "\\1", lastpage))
   
   Linklist = sapply(1:lastpagenumber, getPagelink, lastpagelink = lastpage)
   
@@ -41,8 +39,8 @@ getLinklist.2 = function(url){
 
 
 #scrapping data
-getdatacontent = function(node){
-  vin = xmlAttrs(node)["content"]
+getdatacontent.2 = function(node){
+ xmlAttrs(node)["content"]
 }
 
 
@@ -51,19 +49,19 @@ scrapeInfo.2 <- function(url)
   doc = htmlParse(url)
   
   vin.node = getNodeSet(doc, "//meta[@itemprop='serialNumber']")
-  vins = unique(sapply(vin.node,getdatacontent))
+  vins = unique(sapply(vin.node,getdatacontent.2))
   
   make.node = getNodeSet(doc, "//meta[@itemprop='manufacturer']")
-  make = sapply(make.node,getdatacontent)
+  make = sapply(make.node,getdatacontent.2)
   
   model.node = getNodeSet(doc, "//meta[@itemprop='model']")
-  model = sapply(model.node,getdatacontent)
+  model = sapply(model.node,getdatacontent.2)
   
   year.node = getNodeSet(doc,"//meta[@itemprop='releaseDate']")
-  year = sapply(year.node,getdatacontent)
+  year = sapply(year.node,getdatacontent.2)
   
   trim.node = getNodeSet(doc, "//meta[@itemprop='name']")[-1]
-  name = sapply(trim.node,getdatacontent)
+  name = sapply(trim.node,getdatacontent.2)
   trim = sapply(1:length(name), 
                 function(i){gsub("^\\s+|\\s+$", "", 
                         strsplit(name[i], model[i])$content[2])})
@@ -80,4 +78,4 @@ alldata.2 = function(url){
   return(cardata)
 }
 
-
+#cc = alldata.2(url)
