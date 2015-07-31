82c84889b849e336f72e5dccc5576103c8727abf
jianshi <jshzhang@ucdavis.edu>
2015-07-31 15:23:47 -0700

    /inventory/newsearch/New/

--- Case study-7.R
+++ Case study-7.R
@@ -5,6 +5,7 @@
 #url = "http://www.hondacarsofboston.com/inventory/newsearch/New/"
 
 
+
 getdatacontent.7 = function(node, content){
   tt = xmlAttrs(node)[content]
   return(tt)
@@ -32,25 +33,33 @@ getLinklist.7 = function(url){
 scrapeInfo.7 <- function(url)
 {
   doc = htmlParse(url)
+  print(url)
   nodes = getNodeSet(doc, 
                      "//img[@alt and @id and not(contains(@class, 'vehiclestyleimg'))]")
-  temp = unname(sapply(nodes, getdatacontent.7, content = "alt"))
-  vins = sapply(strsplit(temp, " "), tail, n = 1 )
-##or nodes = getNodeSet(doc, "//img[@alt]");temp = unname(sapply(nodes, getdatacontent.7, content = "alt"))
-##, vins = gsub("(.*)([0-9A-Z]{17})$", "\\2", temp)[grepl("^([0-9A-Z]{17})$", gsub("(.*)([0-9A-Z]{17})$", "\\2", temp))] 
-  make = xpathSApply(doc,"//span[@class='make']",xmlValue)
-  model = xpathSApply(doc,"//span[@class='model']",xmlValue)
-  trim = xpathSApply(doc,"//span[@class='trim']",xmlValue)
-  year = xpathSApply(doc,"//span[@class='year']",xmlValue)
+  if(length(nodes)==0){
+    return(NULL)
+  }
+  else{
+    temp = unname(sapply(nodes, getdatacontent.7, content = "alt"))
+    vins = sapply(strsplit(temp, " "), tail, n = 1 )
+    ##or nodes = getNodeSet(doc, "//img[@alt]");temp = unname(sapply(nodes, getdatacontent.7, content = "alt"))
+    ##, vins = gsub("(.*)([0-9A-Z]{17})$", "\\2", temp)[grepl("^([0-9A-Z]{17})$", gsub("(.*)([0-9A-Z]{17})$", "\\2", temp))] 
+    make = xpathSApply(doc,"//span[@class='make']",xmlValue)
+    model = xpathSApply(doc,"//span[@class='model']",xmlValue)
+    trim = xpathSApply(doc,"//span[@class='trim']",xmlValue)
+    year = xpathSApply(doc,"//span[@class='year']",xmlValue)
+    
+    df <- data.frame(vins,make,model,trim,as.numeric(year), stringsAsFactors = F)
+    colnames(df) <- c("VIN", "Make", "Model", "Trim", "Year")
+    
+    return(df) 
+  }
   
-  df <- data.frame(vins,make,model,trim,as.numeric(year), stringsAsFactors = F)
-  colnames(df) <- c("VIN", "Make", "Model", "Trim", "Year")
-#print(url)
-  return(df)
 } 
 
 #scrape car information from all the pages
 alldata.7 = function(url){
+  
   links = getLinklist.7(url)
   tt = lapply(links, scrapeInfo.7)
   cardata = Reduce(function(x, y) rbind(x, y), tt)
