0e395c3e84d9c1d9fceb678cf57bcdace144c145
jianshi <jshzhang@ucdavis.edu>
2015-08-01 13:05:55 -0700

    search/new/tp/

--- Case study-2.R
+++ Case study-2.R
@@ -1,21 +1,12 @@
-url = "http://www.vacavillehonda.com/search/new/tp/"
+#url = "http://www.vacavillehonda.com/search/new/tp/"
 #doc = htmlParse(url)
 #baselink = substr(url, 1, gregexpr("/",url)[[1]][3]-1)
 
-url = "http://www.harr.com/search/new/tp/"
+#url = "http://www.harr.com/search/new/tp/"
 
 #grab the linklist
 
 #small function to get page links
-getPagelink = function(lastpagelink, pagenumber){
-  lastpagenumber = as.numeric(substr(lastpagelink, gregexpr("p:", lastpagelink)[[1]][2]+2,
-                                     gregexpr("p:", lastpagelink)[[1]][2]+2))
-  pagelink = paste0(strsplit(lastpagelink, lastpagenumber)[[1]][1], pagenumber, 
-                    strsplit(lastpagelink, lastpagenumber)[[1]][2])
-  return(pagelink)
-}
-
-
 getLinklist.2 = function(url){
   baselink = substr(url, 1, gregexpr("/",url)[[1]][3]-1)
   href = unique(getHTMLLinks(url))
@@ -27,8 +18,7 @@ getLinklist.2 = function(url){
   lastpage = paste0(baselink, temp[ind[length(ind)]])
   #get the number of total pages from the last page link
   lastpagenumber = as.numeric(gsub(".*p:([0-9]+).*", "\\1", lastpage))
-  
-  Linklist = sapply(1:lastpagenumber, getPagelink, lastpagelink = lastpage)
+  Linklist = sapply(1:lastpagenumber, function(i)gsub("p:([0-9]+)", paste0("p:", i), lastpage))
   
   return(Linklist)
 }
@@ -50,7 +40,10 @@ scrapeInfo.2 <- function(url)
   
   vin.node = getNodeSet(doc, "//meta[@itemprop='serialNumber']")
   vins = unique(sapply(vin.node,getdatacontent.2))
-  
+  if(length(vins)==0){
+    return(NULL)
+  }
+  else{
   make.node = getNodeSet(doc, "//meta[@itemprop='manufacturer']")
   make = sapply(make.node,getdatacontent.2)
   
@@ -67,7 +60,9 @@ scrapeInfo.2 <- function(url)
                         strsplit(name[i], model[i])$content[2])})
   df <- data.frame(vins,make,model,trim,as.numeric(year), stringsAsFactors = F)
   colnames(df) <- c("VIN", "Make", "Model", "Trim", "Year")
+  print(url)
   return(df)
+  }
 } 
 
 #scrape car information from all the pages
