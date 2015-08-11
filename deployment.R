0570f7f09cf80970974c06ca5edea42b58c0d64e
jpzhangvincent <vincentzhangv@gmail.com>
2015-08-11 14:29:29 -0700

    for RShiny

--- deployment.R
+++ deployment.R
@@ -3,13 +3,15 @@ load('toyota.RData')
 load('smart.RData')
 load('chevrolet.RData')
 load('honda.RData')
+load('ford.RData')
+load('nissan.RData')
 
 nullToNA <- function(x) {
   x[sapply(x, is.null)] <- NA
   return(x)
 }
 
-OEM_list = list(nullToNA(chev_df),nullToNA(toyota_df),nullToNA(smart_df),nullToNA(honda_df))
+OEM_list = list(nullToNA(chev_df),nullToNA(toyota_df),nullToNA(smart_df),nullToNA(honda_df),nullToNA(ford_df),nullToNA(nissan_df))
 df <- Reduce(function(x, y) rbind(x, y), OEM_list)
 df <- df[!duplicated(df$VIN),]
 
