69719d6ee61aeab46d1b2e7b30ba9ea06893f937
jpzhangvincent <vincentzhangv@gmail.com>
2015-08-26 15:20:11 -0700

    clean the dealership info

--- cleanDealerInfo.R
+++ cleanDealerInfo.R
@@ -1,5 +1,5 @@
 #integrte the location information for each dealership
-load("madata.rdata") #example data 
+load("new-madata.rdata") #example data 
 dealer_ls = unique(madata$Dealership)
 length(dealer_ls)
 require(dplyr)
@@ -16,6 +16,14 @@ dealerinfo <- dealerinfo  %>%
   filter(!duplicated(Name))
 #extract the zipcode for each dealerships
 dealerinfo$zipcode = gsub('.*([0-9]{5}).*','\\1',dealerinfo$Address)
+dealerinfo$state = gsub('.* (.*) [0-9]{5}.*','\\1',dealerinfo$Address)
 head(dealerinfo)
+
+#check whether there is any exceptions
+as.numeric(dealerinfo$zipcode)
+#index with 132 has a warning about NA
+dealerinfo[132,]$Address = "765 Memorial Dr, Chicopee, MA 01020"
+dealerinfo[132,]$zipcode = gsub('.*([0-9]{5}).*','\\1',dealerinfo[132,]$Address)
+dealerinfo[132,]$state = gsub('.* (.*) [0-9]{5}.*','\\1',dealerinfo[132,]$Address)
 
 save(dealerinfo,file='dealerinfo.rdata')
