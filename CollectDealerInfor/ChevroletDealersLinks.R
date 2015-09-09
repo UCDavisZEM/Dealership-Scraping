ddd8f43f1c354d92e93e69f45f8d6662adab539b
jpzhangvincent <vincentzhangv@gmail.com>
2015-09-08 18:17:35 -0700

    update

--- CollectDealerInfor/ChevroletDealersLinks.R
+++ CollectDealerInfor/ChevroletDealersLinks.R
@@ -59,6 +59,56 @@ chevroletDealers = ldply(allsearchpages, function(url){
                                                       return(out)
                                                       }, .progress = "text" )
 ChevroletDealers = chevroletDealers[!duplicated(chevroletDealers$Dealer),]
+ChevroletDealers = merge(ChevroletDealers, zipdata)
 save(ChevroletDealers, file="chevroletDealers.rdata")
 
+
 head(ChevroletDealers)
+
+require(RSelenium)
+RSelenium::startServer()
+remDr = remoteDriver(browserName = "firefox")
+remDr$open(silent = TRUE)
+
+remDr$navigate(url) 
+
+webElem <- remDr$findElement(using = 'id',value="zip")
+webElem$sendKeysToElement(list("36801","\uE007"))
+doc <- remDr$getPageSource()[[1]]
+
+getDealerInforInRS = function(doc){
+  doc = htmlParse(doc)
+  dealerNameNodes = getNodeSet(doc,'//div[@class="dealer-name-and-address"]/a[@class="dealer-name"]/span/text()')
+  dealerName = xmlSApply(dealerNameNodes,xmlValue,trim=T)
+  
+  roadNodes = getNodeSet(doc,'//div[@class="dealer-name-and-address"]/div[1]/text()')
+  roadName = xmlSApply(roadNodes,xmlValue,trim=T)[-c(1,2,3,4)]
+  
+  cityNodes = getNodeSet(doc,'//div[@class="cityStateZip"]/span[1]/text()')
+  cityName = xmlSApply(cityNodes,xmlValue,trim=T)
+  
+  stateNodes = getNodeSet(doc,'//div[@class="cityStateZip"]/span[2]/text()')
+  stateName = xmlSApply(stateNodes,xmlValue,trim=T)
+  
+  zipcodeNodes = getNodeSet(doc,'//div[@class="cityStateZip"]/span[3]/text()')
+  zipcode = xmlSApply(zipcodeNodes,xmlValue,trim=T)
+  
+  dealerAddress = paste0(roadName,', ',cityName,', ',stateName,' ',zipcode)
+  
+  dealerWebsiteNodes = getNodeSet(doc,"//div[@class='dealer-name-and-address']/a")
+  dealerWebsite = unlist(lapply(xmlSApply(dealerWebsiteNodes,xmlGetAttr,"href")[-c(1,2)],gsub,patter='(.*/).*',replacement='\\1'))
+  
+  dealerIVwebsiteNodes = getNodeSet(doc,"//a[contains(./text(),'View Inventory')]")
+  dealerIVWebsite = unlist(lapply(xmlSApply(dealerIVwebsiteNodes,xmlGetAttr,"href"),gsub,patter='(.*)referrer.*',replacement='\\1'))
+  dealerInventoryLink = paste0(dealerIVWebsite,'search=new')
+  
+  GeoNodes = getNodeSet(doc,'//div[@class="dealer-listing-item"]')
+  Latitude = xmlSApply(GeoNodes,xmlGetAttr,"data-latitude")
+  Longitude = xmlSApply(GeoNodes,xmlGetAttr,"data-longitude")
+  
+  df <- data.frame(dealerName, dealerAddress, dealerWebsite, zipcode, dealerInventoryLink, Latitude, Longitude, stringsAsFactors=F )
+  colnames(df) = c("Dealer","Address","Link","zipcode","IV_link", "Latitude", "Longitude")
+  return(df)
+}
+
+testdf = getDealerInforInRS(doc)
